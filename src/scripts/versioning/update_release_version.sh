d86a1e3ef27a39cf8aec215e91703b4cf626f32c
sebeifares <fares.sebei@graphmasters.net>
2022-11-23 11:44:22 +0100

    #16 fix tag name in update_release_version script

--- src/scripts/versioning/update_release_version.sh
+++ src/scripts/versioning/update_release_version.sh
@@ -27,7 +27,7 @@ checkRequirements() {
 getLastVersion (){
   # Extract release version from the last created tag
   lastUsedTag=$(git tag -l "${!GITHUB_TAG}" --sort -version:refname  | head -n 1)
-  releaseVersion=$(echo "${lastUsedTag}" | rev | cut -d'-' -f 1 | rev )
+  releaseVersion=$(echo "${lastUsedTag}-*" | rev | cut -d'-' -f 1 | rev )
   echo "${releaseVersion-"0.0.1"}"
 }
 
